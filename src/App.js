f48a7c89d13e4e4fd2a91c646f9c6e13ddbe0df5
Kevin Jake <kevin_jake_04@yahoo.com>
2022-06-19 23:22:04 +0800

    add live voltage reading on table

--- src/App.js
+++ src/App.js
@@ -4,11 +4,18 @@ import { a, Amplify, API, graphqlOperation } from "aws-amplify";
 import config from "./aws-exports";
 import { listTodos } from "./graphql/queries";
 import { onCreateTodo } from "./graphql/subscriptions";
+import { Category, ChartComponent, Inject, LineSeries, SeriesCollectionDirective, SeriesDirective, Legend, DataLabel, Tooltip } from '@syncfusion/ej2-react-charts'
+
 
 Amplify.configure(config);
 
 function App() {
   const [acPower, setacPower] = useState([]);
+
+  useEffect(() => {
+    fetchACPower()
+  }, [])
+  
 
   useEffect(() => {
     const subscription = API.graphql(
@@ -21,24 +28,25 @@ function App() {
           },
         } = data;
         const acData = [...acPower, onCreateTodo];
+        acData.shift()
         setacPower(acData);
       },
     });
     return () => subscription.unsubscribe();
   }, [acPower]);
 
-  // const fetchACPower = async () => {
-  //   const apiData = await API.graphql({
-  //     query: listTodos,
-  //   });
-  //   setacPower(apiData.data.listTodos.items);
-  // };
+  const fetchACPower = async () => {
+    const apiData = await API.graphql({
+      query: listTodos,
+    });
+    setacPower(apiData.data.listTodos.items);
+  };
 
   console.log(acPower);
 
   return (
     <div className="App">
-      {acPower?.map((item, index) => (
+      {/* {acPower?.map((item, index) => (
         <div key={item.id}>
           <h4>Index: {index}</h4>
           <h4>{item.id}</h4>
@@ -46,7 +54,17 @@ function App() {
           <p>{item.voltage}</p>
           <p>{item.power}</p>
         </div>
-      ))}
+      ))} */}
+      <ChartComponent primaryXAxis={{valueType:"Category", title:"Time"}} primaryYAxis={{title: "Voltage", minimum: 0, maximum: 300}} legendSettings={{visible:true}} tooltip={{enable:true}}>
+        <Inject services={[LineSeries, Category, Legend, DataLabel, Tooltip]}></Inject>
+        <SeriesCollectionDirective>
+          <SeriesDirective type="Line" dataSource={acPower} xName="createdAt" yName="voltage" name="Voltage"
+          marker={{dataLabel:{visible: true}, visible: true}}
+          >
+
+          </SeriesDirective>
+        </SeriesCollectionDirective>
+      </ChartComponent>
     </div>
   );
 }
